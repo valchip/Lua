763484617d423aa745478a2f61a46d58278b8579
vamik64 <45536276+vamik64@users.noreply.github.com>
2019-04-24 17:49:03 +0200

    Update NoCacheRequire.lua

--- NoCacheRequire.lua
+++ NoCacheRequire.lua
@@ -1,8 +1,11 @@
 --Requires a module by its asset id without caching. Make sure you change the "moduleName" to the name of your own module!
 
 local function NoCacheRequire(id)
+	local succ, err = pcall(function()
 	local LatestVersionOfAsset = game:GetService("InsertService"):GetLatestAssetVersionAsync(id)
-	local asset = game:GetService("InsertService"):LoadAssetVersion(LatestVersionOfAsset)
+	asset = game:GetService("InsertService"):LoadAssetVersion(LatestVersionOfAsset)
+	end)
+	if not succ then return "error" end
 	local module = asset:FindFirstChildOfClass("ModuleScript")
 	asset:Destroy()
 	return require(module)
