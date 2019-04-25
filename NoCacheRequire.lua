3c44ee5bfc63d2bfc23c13e7abbb8d4e3b32391c
vamik64 <45536276+vamik64@users.noreply.github.com>
2019-04-25 17:46:27 +0200

    Update NoCacheRequire.lua

--- NoCacheRequire.lua
+++ NoCacheRequire.lua
@@ -1,13 +1,43 @@
---Requires a module by its asset id without caching.
+--[[
+Non caching require module function.
+Programmed by vamik64.
 
-local function NoCacheRequire(id)
+Usage:
+local Require = require(Require)
+local ARandomModule = Require(moduleIdHere)
+
+or
+
+local Require = require(03111497486)
+local ARandomModule = Require(moduleIdHere)
+
+Supports modules being required by their path too, but they cache as
+I did not see any reason to make modules that are required by their path not cache.
+--]]
+
+local lastVersion;
+local lastRequiredModule;
+	
+return function(id)
+	if type(id) == "number" then
 	local succ, err = pcall(function()
-	local LatestVersionOfAsset = game:GetService("InsertService"):GetLatestAssetVersionAsync(id)
+	LatestVersionOfAsset = game:GetService("InsertService"):GetLatestAssetVersionAsync(id)
 	asset = game:GetService("InsertService"):LoadAssetVersion(LatestVersionOfAsset)
 	end)
-	if not succ then return "error" end
+	if (lastVersion == nil) then
+		lastVersion = LatestVersionOfAsset
+	elseif (lastVersion == LatestVersionOfAsset) then 
+		return lastRequiredModule
+	end	
 	local module = asset:FindFirstChildOfClass("ModuleScript")
 	if not succ or module == nil then return "error" end
 	asset:Destroy()
-	return require(module)
+	local reqModule = require(module)
+	lastRequiredModule = reqModule
+	return reqModule
+	elseif id:IsA("ModuleScript") then
+		return require(id)
+	else
+		warn("Invalid argument.")
+	end		
 end
